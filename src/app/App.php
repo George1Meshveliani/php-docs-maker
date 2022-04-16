e93ca6271e8df812cd45b0cf6d2e3b5d214100e2
George1Meshveliani <giorgi.meshveliani@gmail.com>
2022-04-17 01:59:44 +0400

    validate data with colors based on amount

--- src/app/App.php
+++ src/app/App.php
@@ -16,23 +16,38 @@ if (($open = fopen(FILE_PATH, "r")) !== FALSE) {
  */
 function getData($array_name) {
     foreach ($array_name as $arr) {
+
         if ($arr[0] != 'Date') {
+
             $date = $arr[0];
             $formated_date =  date('F d, Y', strtotime($date));
+            $check = $arr[1];
+            $transaction = $arr[2];
+            $amount = $arr[3];
+            $amount_number = str_replace('$', '', $arr[3]);
+
+            if ($amount_number > 0) {
+                $amounts = "<td style='color: limegreen'> $amount </td>";
+                $transactions = "<td style='color: limegreen'> $transaction </td>";
+                $checks = "<td style='color: limegreen'> $check </td>";
+                $given_date = "<td style='color: limegreen'> $formated_date </td>";
+            }
+            else {
+                $amounts = "<td style='color: red'> $amount </td>";
+                $transactions = "<td style='color: red'> $transaction </td>";
+                $checks = "<td style='color: red'> $check </td>";
+                $given_date = "<td style='color: red'> $formated_date </td>";
+            }
             echo "
                   <tr> 
-                    <td> $formated_date </td> 
-                    <td> $arr[1] </td> 
-                    <td> $arr[2] </td> 
-                    <td> $arr[3] </td> 
+                    $given_date
+                    $checks
+                    $transactions 
+                    $amounts
                   </tr>          
                   ";
         }
     }
-    echo "<pre>";
-    var_dump($date);
-    var_dump($formated_date);
-    echo "</pre>";
 }
 
 ?>
