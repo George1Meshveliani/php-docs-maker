40104c4b606edea2a23fedb9d4714452c677f499
George1Meshveliani <giorgi.meshveliani@gmail.com>
2022-05-03 01:11:04 +0400

    make it OOP

--- src/app/App.php
+++ src/app/App.php
@@ -1,44 +1,45 @@
 <?php
 
-const FILE_PATH = "../app/transaction_files/sample_1.csv";
+namespace App;
 
-if (($open = fopen(FILE_PATH, "r")) !== FALSE) {
+class App {
 
-    while (($data = fgetcsv($open, 1000)) !== FALSE) {
-        $array[] = $data;
+    private array $array_name;
+    private string $result;
+
+    public function __construct($array_name, $result) {
+        $this->array_name = $array_name;
+        $this->result = $result;
     }
+    /**
+     * @param $array_name
+     */
+    function getData() {
 
-    fclose($open);
-}
+        foreach ($this->array_name as $arr) {
 
-/**
- * @param $array_name
- */
-function getData($array_name) {
-    foreach ($array_name as $arr) {
+            if ($arr[0] != 'Date') {
 
-        if ($arr[0] != 'Date') {
+                $date = $arr[0];
+                $formated_date =  date('F d, Y', strtotime($date));
+                $check = $arr[1];
+                $transaction = $arr[2];
+                $amount = $arr[3];
+                $amount_number = str_replace('$', '', $arr[3]);
 
-            $date = $arr[0];
-            $formated_date =  date('F d, Y', strtotime($date));
-            $check = $arr[1];
-            $transaction = $arr[2];
-            $amount = $arr[3];
-            $amount_number = str_replace('$', '', $arr[3]);
-
-            if ($amount_number > 0) {
-                $amounts = "<td style='color: limegreen'> $amount </td>";
-                $transactions = "<td style='color: limegreen'> $transaction </td>";
-                $checks = "<td style='color: limegreen'> $check </td>";
-                $given_date = "<td style='color: limegreen'> $formated_date </td>";
-            }
-            else {
-                $amounts = "<td style='color: red'> $amount </td>";
-                $transactions = "<td style='color: red'> $transaction </td>";
-                $checks = "<td style='color: red'> $check </td>";
-                $given_date = "<td style='color: red'> $formated_date </td>";
-            }
-            echo "
+                if ($amount_number > 0) {
+                    $amounts = "<td style='color: limegreen'> $amount </td>";
+                    $transactions = "<td style='color: limegreen'> $transaction </td>";
+                    $checks = "<td style='color: limegreen'> $check </td>";
+                    $given_date = "<td style='color: limegreen'> $formated_date </td>";
+                }
+                else {
+                    $amounts = "<td style='color: red'> $amount </td>";
+                    $transactions = "<td style='color: red'> $transaction </td>";
+                    $checks = "<td style='color: red'> $check </td>";
+                    $given_date = "<td style='color: red'> $formated_date </td>";
+                }
+                echo "
                   <tr> 
                     $given_date
                     $checks
@@ -46,41 +47,44 @@ function getData($array_name) {
                     $amounts
                   </tr>          
                   ";
-        }
-    }
-}
-
-/**
- * @param $array_name
- * @param string $result
- */
-function getTotal($array_name, $result = '') {
-    $sum = 0;
-    if ($result == 'income') {
-        foreach ($array_name as $arr) {
-            $amount_number = intval(str_replace('$', '', $arr[3]));
-            if ($amount_number > 0) {
-                $sum += $amount_number;
             }
         }
     }
 
-    if ($result == 'expenses') {
-        foreach ($array_name as $arr) {
-            $amount_number = intval(str_replace('$', '', $arr[3]));
-            if ($amount_number < 0) {
-                $sum -= $amount_number;
+    /**
+     * @param $array_name
+     * @param string $result
+     */
+    function getTotal() {
+        $sum = 0;
+        if ($this->result == 'income') {
+            foreach ($this->array_name as $arr) {
+                $amount_number = intval(str_replace('$', '', $arr[3]));
+                if ($amount_number > 0) {
+                    $sum += $amount_number;
+                }
             }
         }
+
+        if ($this->result == 'expenses') {
+            foreach ($this->array_name as $arr) {
+                $amount_number = intval(str_replace('$', '', $arr[3]));
+                if ($amount_number < 0) {
+                    $sum -= $amount_number;
+                }
+            }
+        }
+
+        if ($this->result == 'net') {
+            foreach ($this->array_name as $arr) {
+                $amount_number = intval(str_replace('$', '', $arr[3]));
+                $sum += abs($amount_number);
+            }
+        }
+        return $sum;
     }
 
-    if ($result == 'net') {
-        foreach ($array_name as $arr) {
-            $amount_number = intval(str_replace('$', '', $arr[3]));
-            $sum += abs($amount_number);
-        }
-    }
-    return $sum;
 }
+
 
 ?>
